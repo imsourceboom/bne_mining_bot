dca37707ee5da5dab7831ab9c7a854ec63a1e36d
imsourceboom <ecmascript.developer@gmail.com>
2021-10-10 06:13:12 +0900

    modified path

--- utils/nextCycle.js
+++ utils/nextCycle.js
@@ -2,7 +2,7 @@ const shell = require('shelljs');
 const { dateFormat } = require('./dateFormat');
 
 exports.nextCycle = () => {
-  const cycleStart = shell.exec('bash ssh.sh | tail -n 3 | awk "FNR == 1"');
+  const cycleStart = shell.exec('bash ../ssh.sh | tail -n 3 | awk "FNR == 1"');
   // const cycleStart = shell.exec('bash getData.sh | awk "FNR == 1"').stdout;
   const currentNow = Math.floor(+new Date() / 1000);
   let since = parseInt(cycleStart) + 65536;
